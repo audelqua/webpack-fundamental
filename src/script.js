6fd0861b626873b35e53ac54a9271bb117440b26
audrey <mbozorgi604@gmail.com>
2023-01-31 13:55:53 +0330

    src and root js file added beside adding loadash package

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function component() {
+    const element = document.getElementById('div')
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+
+    return element 
+}
+
+document.body.appendChild(component())
